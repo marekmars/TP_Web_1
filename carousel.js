a7040c94461eed6d8f6bdd3eb12c41f2063dcba3
Marco Silva <nelsonmarcosilva@gmail.com>
2022-10-10 11:25:02 -0300

    commit cambio js

---
+++ carousel.js
@@ -0,0 +1,69 @@
+const carouselSlide = () => {
+    //definicion de constantes
+    let slidePosition = 0;
+    const slides = document.getElementsByClassName("carousel__img");
+    const totalSlides = slides.length - 1;
+    /*console.log(totalSlides);*/
+    const pButton = document.querySelector(".carousel__button--prev");
+    const nButton = document.querySelector(".carousel__button--next");
+    const carouselIndicators = document.getElementsByClassName("carousel__indicator");
+    const dots=Array.from(carouselIndicators);
+
+    //mover slides para adelante
+    pButton.addEventListener("click", (e) => {
+        if (slidePosition === 0) {
+            slidePosition = totalSlides;
+        } else {
+            slidePosition--;
+        }
+        slides[slidePosition].classList.remove("animacionDerecha");
+        slides[slidePosition].classList.add("animacionIzquierda");
+        moveSlide();
+    })
+    //mover slides para atras
+    nButton.addEventListener("click", (e) => {
+        if (slidePosition === totalSlides) {
+            slidePosition = 0;
+        } else {
+            slidePosition++;
+        }
+        slides[slidePosition].classList.remove("animacionIzquierda");
+        slides[slidePosition].classList.add("animacionDerecha");
+        moveSlide();
+    })
+//cambio de clase
+    function moveSlide() {
+        for (let slide of slides) {
+            slide.classList.remove("carousel__img-visible");
+            slide.classList.add("carousel__img-hidden");
+            slides[slidePosition].classList.add("carousel__img-visible");
+        }
+        for (let dot of carouselIndicators) {
+            dot.classList.remove("carousel__indicator__active");
+            carouselIndicators[slidePosition].classList.add("carousel__indicator__active");
+        }
+    }
+    //cambio de clase indicadores + accion botones de los mismos
+    const carouselIndicatorsClick = () => {
+        for (let i = 0; i <dots.length ; i++) {
+            dots[i].addEventListener("click", evt => {
+                /*console.log("current "+slidePosition)*/
+                /*console.log("taret "+i);*/
+                let current=slidePosition;
+                //se le asigna el valor de i del boton apretado a slideposition
+                slidePosition=i;
+                //colocando animacion dependiento del lado q este el indicador de destino
+                if(current>slidePosition){
+                    slides[slidePosition].classList.remove("animacionIzquierda");
+                    slides[slidePosition].classList.add("animacionDerecha");
+                }else if(current<slidePosition) {
+                    slides[slidePosition].classList.remove("animacionDerecha");
+                    slides[slidePosition].classList.add("animacionIzquierda");
+                }
+                moveSlide();
+            })
+        }
+    }
+    carouselIndicatorsClick();
+}
+carouselSlide();
---
+++ form.js
@@ -0,0 +1,79 @@
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const msg = document.getElementById("msg");
+
+const arrayForm = [nombre, email, phone, msg];
+const form = document.getElementById("formu");
+const error = document.getElementsByClassName("error");
+const okIcon = document.getElementsByClassName("fa-circle-check");
+const wrongIcon = document.getElementsByClassName("fa-circle-xmark");
+let flag = true;
+
+const errorMsg = [
+  "El nombre no puede superara los 30 caracteres",
+  "Ingrese un mail correcto",
+  "Ingrese un numero correcto",
+  "El mensaje no puede superara los 150 caracteres",
+];
+
+let verificationOk = (i) => {
+  error[i].innerText = "";
+  wrongIcon[i].style.opacity = "0";
+  okIcon[i].style.opacity = "1";
+  flag = true;
+};
+
+let verificationEmpty = () => {
+  arrayForm.forEach((element, i) => {
+    if (element.value === "") {
+      okIcon[i].style.opacity = "0";
+      wrongIcon[i].style.opacity = "0";
+      error[i].innerText = "";
+      flag=false;
+    }
+  });
+};
+
+let verificationWrong = (i) => {
+  error[i].innerText = errorMsg[i];
+  wrongIcon[i].style.opacity = "1";
+  okIcon[i].style.opacity = "0";
+  flag = false;
+};
+
+form.addEventListener("change", (e) => {
+  
+  for (let i = 0; i < 4; i++) {
+    if (arrayForm[i].value.length > 30) {
+      verificationWrong(i);
+    } else {
+      verificationOk(i);
+    }
+  }
+
+  if (isNaN(arrayForm[2].value)||arrayForm[2].value.length<8) {
+    verificationWrong(2);
+  } else {
+    verificationOk(2);
+  } 
+
+  if (
+    !arrayForm[1].value.includes("@") ||
+    !arrayForm[1].value.includes(".com")
+  ) {
+    verificationWrong(1);
+  } else {
+    verificationOk(1);
+  }
+  
+  verificationEmpty();
+  console.log(flag);
+});
+
+form.addEventListener("submit", (e) => {
+  if (!flag) {
+    console.log("Error");
+    e.preventDefault();
+  }
+});
