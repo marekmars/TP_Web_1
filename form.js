e7fbe4be915f02209a272ef596ec0c7135b6d4fe
Marco Silva <nelsonmarcosilva@gmail.com>
2022-10-10 11:32:55 -0300

    commit cambio js

--- form.js
+++ form.js
@@ -11,69 +11,70 @@ const wrongIcon = document.getElementsByClassName("fa-circle-xmark");
 let flag = true;
 
 const errorMsg = [
-  "El nombre no puede superara los 30 caracteres",
-  "Ingrese un mail correcto",
-  "Ingrese un numero correcto",
-  "El mensaje no puede superara los 150 caracteres",
+    "El nombre no puede superara los 30 caracteres",
+    "Ingrese un mail correcto",
+    "Ingrese un numero correcto",
+    "El mensaje no puede superara los 150 caracteres",
+    "El campo no puede estar vacio"
 ];
 
 let verificationOk = (i) => {
-  error[i].innerText = "";
-  wrongIcon[i].style.opacity = "0";
-  okIcon[i].style.opacity = "1";
-  flag = true;
+    error[i].innerText = "";
+    wrongIcon[i].style.opacity = "0";
+    okIcon[i].style.opacity = "1";
+    flag = true;
 };
 
 let verificationEmpty = () => {
-  arrayForm.forEach((element, i) => {
-    if (element.value === "") {
-      okIcon[i].style.opacity = "0";
-      wrongIcon[i].style.opacity = "0";
-      error[i].innerText = "";
-      flag=false;
-    }
-  });
+    arrayForm.forEach((element, i) => {
+        if (element.value === "") {
+            okIcon[i].style.opacity = "0";
+            wrongIcon[i].style.opacity = "0";
+            error[i].innerText = errorMsg[4];
+            flag = false;
+        }
+    });
 };
 
 let verificationWrong = (i) => {
-  error[i].innerText = errorMsg[i];
-  wrongIcon[i].style.opacity = "1";
-  okIcon[i].style.opacity = "0";
-  flag = false;
+    error[i].innerText = errorMsg[i];
+    wrongIcon[i].style.opacity = "1";
+    okIcon[i].style.opacity = "0";
+    flag = false;
 };
 
 form.addEventListener("change", (e) => {
-  
-  for (let i = 0; i < 4; i++) {
-    if (arrayForm[i].value.length > 30) {
-      verificationWrong(i);
-    } else {
-      verificationOk(i);
+
+    for (let i = 0; i < 4; i++) {
+        if (arrayForm[i].value.length > 30) {
+            verificationWrong(i);
+        } else {
+            verificationOk(i);
+        }
     }
-  }
 
-  if (isNaN(arrayForm[2].value)||arrayForm[2].value.length<8) {
-    verificationWrong(2);
-  } else {
-    verificationOk(2);
-  } 
+    if (isNaN(arrayForm[2].value) || arrayForm[2].value.length < 8) {
+        verificationWrong(2);
+    } else {
+        verificationOk(2);
+    }
 
-  if (
-    !arrayForm[1].value.includes("@") ||
-    !arrayForm[1].value.includes(".com")
-  ) {
-    verificationWrong(1);
-  } else {
-    verificationOk(1);
-  }
-  
-  verificationEmpty();
-  console.log(flag);
+    if (
+        !arrayForm[1].value.includes("@") ||
+        !arrayForm[1].value.includes(".com")
+    ) {
+        verificationWrong(1);
+    } else {
+        verificationOk(1);
+    }
+
+    verificationEmpty();
+    console.log(flag);
 });
 
 form.addEventListener("submit", (e) => {
-  if (!flag) {
-    console.log("Error");
-    e.preventDefault();
-  }
+    if (!flag) {
+        console.log("Error");
+        e.preventDefault();
+    }
 });
