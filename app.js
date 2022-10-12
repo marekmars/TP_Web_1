59bb17ee97d8fa25350def99c3f5e035d0e6a22d
Marco Silva <nelsonmarcosilva@gmail.com>
2022-10-12 15:05:17 -0300

    cambio en conocenos

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const navSlide = () => {
     //cambio de nav
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-        //animar lnks
-        navLinks.forEach((link, i) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${i / 7 + 0.5}s`;
-            }
-        });
         //animar burger
         burger.classList.toggle('animateB');
     });
